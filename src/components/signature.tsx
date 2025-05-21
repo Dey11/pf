b5dc65df211138d29aa018b34d0e91c0b3dc22db
Dey11 <shreyandey11@gmail.com>
2025-05-22 01:37:24 +0530

    make signature component

---
+++ src/components/signature.tsx
@@ -0,0 +1,15 @@
+export default function Signature() {
+  return (
+    <div className="relative flex h-fit w-fit flex-col leading-none">
+      <span className="absolute left-0 block text-[4svw] sm:text-[4svh]">
+        i'm
+      </span>
+      <p className="block text-[10svw] font-bold sm:text-[10svh]">
+        shreyan <span className="text-secondary">dey</span>
+      </p>
+      <span className="absolute right-[5svw] -bottom-[2svw] block sm:right-[5svh] sm:-bottom-[2svh] sm:text-[4svh]">
+        a fullstack dev
+      </span>
+    </div>
+  );
+}
